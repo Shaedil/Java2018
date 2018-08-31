e221b3149d6c2f9ea0bc0f95f14c1cda8a6f9752
Shaedil Dider <shaedildider@gmail.com>
2018-08-30 20:53:13 -0400

    Fixed letter matching and score updater

--- Java_files/hangman.java
+++ Java_files/hangman.java
@@ -13,32 +13,40 @@
  *------------------------------------------------------------------------
  */
 
+import java.util.Scanner;
 import java.util.Random;
 import java.util.Arrays;
 import java.io.*;
-class hangman {
-    public static int score, guess;
+public class hangman {
+
+    public static int score;
+    public static int guess;
+    public static Scanner scanner;
+
     public static void main(String[] args) {
         score = guess = 0;
-        Scanner scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in);
+
         String word = getRandomWord();
         int lengthOfWord = word.length();
+
         printInfo(word, lengthOfWord); //Includes gameRules, gameLegal & gameInfo all in 1.
         startGame(word, lengthOfWord);
         endGame(word, lengthOfWord); //Print statements.
-    
+        
+        scanner.close(); 
     }
     
     public static void printInfo(String word, int lengthOfWord) {
         
         System.out.println("You are now playing: Hangman.");
         System.out.println("You have 20 guesses to guess the word.");
-        System.out.println("The word is ");
-        System.out.print(lengthOfWord);
-        System.out.print(" characters long. ");
+        System.out.println("The word is " + lengthOfWord + " characters long.");
+        System.out.println("Instances begin on 0 = first letter of word & 1 = second letter of word");
         System.out.println("Guess the word before you run out of guesses.");
         System.out.println("TIP: Don't guess the same letter twice else!\n\n");
     }
+
     public static String getRandomWord() {
         
         String[] wordList = {"AWKWARD", "BAGPIPES", "BANJO", "BUNGLER", "CROQUET", "CRYPT", "DWARVES", "FERVID", "FISHHOOK", "FJORD", "GAZEBO", "GYPSY", "HAIKU", "HAPHAZARD", "HYPHEN", "IVORY", "JAZZY", "JIFFY", "JINX", "JUKEBOX", "KAYAK", "KIOSK", "KLUTZ", "MEMENTO", "MYSTIFY", "NUMBSKULL", "OSTRACIZE", "OXYGEN", "PAJAMA", "PHLEGM", "PIXEL", "POLKA", "QUAD", "QUIP", "RHYTHMIC", "ROGUE", "SPHINX", "SQUAWK", "SWIVEL", "TOADY", "TWELFTH", "UNZIP", "WAXY", "WILDEBEEST", "YACHT", "ZEALOUS", "ZIGZAG", "ZIPPY", "ZOMBIE", "CANDY"};
@@ -49,90 +57,102 @@ class hangman {
     public static void startGame(String word, int lengthOfWord ) {
         //Create the blank spaces for each letter
         for (int i = 0; i < lengthOfWord; i++) {
-            System.out.print("_ ");
+            System.out.print("_ " );
         }
-        int guess = 0;
+        //DEBUG PLEASE REMOVE AFTER YOU'RE DONE THANK YOU!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        System.out.println(word);
+        
         //Game mechanics
         for (int j = 0; j < 20; j++) {
-            char letter = getLetter();
-            boolean isLetterCorrect = checkIfLetterIsCorrect(letter, word, lengthOfWord);
-            fillLetterInBlank(letter, word, lengthOfWord, isLetterCorrect);
-            guess++;
-            System.out.println("# of Guesses: " + guess);
-            System.out.println("Score: " + score);
-            if (score >= lengthOfWord) {
+            //Retriever and Check
+            String guessWord = getWord();
+            boolean winOrLoss = checkIfWordComplete(guessWord, word, lengthOfWord); 
+            if (winOrLoss = true) {
                 break;
             }
-            else {
-                continue;
+
+            char letter = getLetter();
+            int point = checkIfLetterIsCorrect(letter, word, lengthOfWord);
+            
+            //Score & guess.
+            if (point == 1) {
+                score += point;
             }
+                
+            guess++;
+
+            //Print current game info
+            System.out.println("# of Guesses: " + guess);
+            System.out.println("Score: " + score);
+            
+            for (int l = -1; (l = word.indexOf(letter, l + 1)) != -1; l++) {
+                System.out.println("The instances of this letter are on letter # " + l);
+            }
+
+            System.out.println("-------------------------------");
         }
-    }
-    public static void fillLetterInBlank(char letter, String word, int lengthOfWord, boolean isLetterCorrect) {
-        if (isLetterCorrect = true) {
-            for (int i = 0; i > lengthOfWord; i++) {
-                if (word.charAt(i) == letter) {
-                    System.out.print(word.charAt(i));
-                }
-            } 
-        } 
-        else {
-            System.out.println("Not a letter in the word.");
-        }
-    }
+    } //end startGame method
+
     public static char getLetter() {
         
-        Scanner scanner = new Scanner(System.in);
         System.out.print("Guess a letter: ");
         String input = scanner.nextLine();
         input = input.toUpperCase(); //to turn all letters to uppercase so that it is easy to match letter to letter.
         return input.charAt(0); //returns the first letter of the input like an array.
 
     }
+    public static String getWord() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Guess the word: ");
+        String input = in.next();
+        return input;
+    }
 
-    public static boolean checkIfLetterIsCorrect(char letter, String word, int lengthOfWord) {
-        
-        for (int i = 0; i > lengthOfWord; i++) {
+    public static int checkIfLetterIsCorrect(char letter, String word, int lengthOfWord) {
+        /*for (int i = 0; i < lengthOfWord; i++) {
             if (word.charAt(i) == letter) {
                 score++;
-                break;
-            }
-            else {
-                return false;
             }
         }
-        return true;
+        */
+        for (int l = -1; (l = word.indexOf(letter, l + 1)) != -1; l++) {
+            score++;
+        }
+        return score;
+    }
+    
+    public static boolean checkIfWordComplete(String guessWord, String word, int lengthOfWord) {
+        if (guessWord.equals(word)) {
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 
     public static void endGame(String word, int lengthOfWord) {
 
-       if (score == lengthOfWord) {
-          if (guess <= 10) {
-              try {
-                  System.out.println("You won? Wow, that was fast! You deserve a trophy!");
-                  Thread.sleep(3000);
-                  System.out.println("   ._________.");
-                  System.out.println(" __[m        ]__");
-                  System.out.println("(  (m        )  )");
-                  System.out.println(" `-_(m      )_-\u0301");
-                  System.out.println("     (m    )    ");
-                  System.out.println("    ,-(m  )-、  ");
-                  System.out.println("   /Congrats!\\  ");
-                  System.out.println("  /___________\\ ");
-              }
-              catch (InterruptedException ex) {Thread.currentThread().interrupt();}
-          }
-          else {
-              System.out.println("You won! Nice job!");
-          }
-       } 
-       else {
-          System.out.println("Ahh too bad, better luck next time yeah?");
-       }
+        if (guess <= 10) {
+            try {
+                System.out.println("You won? Wow, that was fast! You deserve a trophy!");
+                Thread.sleep(1000);
+                System.out.println("   ._________.");
+                System.out.println(" __[m        ]__");
+                System.out.println("(  (m        )  )");
+                System.out.println(" `-_(m      )_-\u0301");
+                System.out.println("     (m    )    ");
+                System.out.println("    ,-(m  )-、  ");
+                System.out.println("   /Congrats!\\  ");
+                System.out.println("  /___________\\ ");
+            }
+            catch (InterruptedException ex) {Thread.currentThread().interrupt();}
+        }
+        else {
+            System.out.println("You won! Nice job!");
+        }
        
-       System.out.println("The word was " + word + "\n Good game!");
-       System.out.flush();
+        System.out.println("The word was " + word + "\nGood game!");
+        System.out.flush();
     
     }
-   
 } //Ends class hangman
